3f65fc334afba2dfdb8e56a6f86daaa0bf1657da
ErfanBagheri404 <mrenmityerf@gmail.com>
2026-09-22 00:52:44 +0330

    build: version 0.1.2 (0.0.2 was a downgrade from 0.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 3
-        versionName = "0.0.2"
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations += listOf("fa")
